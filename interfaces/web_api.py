fbef9ce0ded2fce1fb60d30c276934231bf38fea
Max Netterberg <max.netterberg@gmail.com>
2020-10-01 21:11:35 +0200

    file serving is nearly working

--- interfaces/web_api.py
+++ interfaces/web_api.py
@@ -1,12 +1,43 @@
-from flask import Flask
+from flask import Flask, request, flash, redirect, url_for, send_from_directory
 from flask_cors import CORS
+import uuid
+import os
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
 CORS(app)
 app.config["DEBUG"] = True
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-@app.route('/', methods=['GET'])
-def home():
-    return "Hello world"
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-app.
+def secure_filename(filename):
+    return str(uuid.uuid4()) + filename
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+@app.route('/', methods=['POST'])
+def cut():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+
+    file = request.files['file']
+
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('uploaded_file', filename=filename))
+    # Cut logic here
+    return "this is cut"
